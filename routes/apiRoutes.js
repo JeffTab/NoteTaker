a501834977772797d1f98acfc699a91faeb7a165
Jeff Tab <j.tab.jr@gmail.com>
2020-05-11 01:04:26 -0400

    finished again

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,12 +1,12 @@
 const router = require("express").Router();
-const noteData = require("../db/db.json");
+const notes = require("../db/db.json");
 
 router.get("/api/notes", (req, res) => {
     res.json(notes);
 });
 
 router.post("/api/notes", (req, res) => {
-    req.body.id = noteData.length;
+    req.body.id = notes.length;
     console.log(req.body);
     notes.push(req.body);
     res.json(req.body);
